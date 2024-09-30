a1a17048fa9f992db8467d5825518579a143d958
Frank Lesniak <franklesniak@hotmail.com>
2024-09-30 17:54:48 -0500

    Initial edits to demo script

--- Win10_IoT_Core_Config_Demo.ps1
+++ Win10_IoT_Core_Config_Demo.ps1
@@ -42,6 +42,339 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #endregion License ################################################################
 
+#region Part 0: Load required functions ############################################
+function Get-FolderPathContainingScript {
+    # Returns the path to the folder containing the running script. If no script is
+    # running, the function returns the current path
+    #
+    # Example:
+    # Get-FolderPathContainingScript
+    #
+    # This example returns the folder that contains the currently running script. Or,
+    # if no script is running, the function returns the current folder
+    #
+    # The function outputs a [string] object representing the path to the folder
+    #
+    # PowerShell v1 - v2 do not have a $PSScriptRoot variable, so this function uses
+    # other methods to determine the script directory
+    #
+    # Version 1.0.20240930.1
+
+    #region License ############################################################
+    # Copyright (c) 2024 Frank Lesniak
+    #
+    # Permission is hereby granted, free of charge, to any person obtaining a copy
+    # of this software and associated documentation files (the "Software"), to deal
+    # in the Software without restriction, including without limitation the rights
+    # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    # copies of the Software, and to permit persons to whom the Software is
+    # furnished to do so, subject to the following conditions:
+    #
+    # The above copyright notice and this permission notice shall be included in
+    # all copies or substantial portions of the Software.
+    #
+    # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    # SOFTWARE.
+    #endregion License ############################################################
+
+    #region DownloadLocationNotice #############################################
+    # The most up-to-date version of this script can be found on the author's
+    # GitHub repository at https://github.com/franklesniak/PowerShell_Resources
+    #endregion DownloadLocationNotice #############################################
+
+    function Get-PSVersion {
+        # Returns the version of PowerShell that is running, including on the original
+        # release of Windows PowerShell (version 1.0)
+        #
+        # Example:
+        # Get-PSVersion
+        #
+        # This example returns the version of PowerShell that is running. On versions
+        # of PowerShell greater than or equal to version 2.0, this function returns the
+        # equivalent of $PSVersionTable.PSVersion
+        #
+        # The function outputs a [version] object representing the version of
+        # PowerShell that is running
+        #
+        # PowerShell 1.0 does not have a $PSVersionTable variable, so this function
+        # returns [version]('1.0') on PowerShell 1.0
+        #
+        # Version 1.0.20240917.0
+
+        #region License ############################################################
+        # Copyright (c) 2024 Frank Lesniak
+        #
+        # Permission is hereby granted, free of charge, to any person obtaining a copy
+        # of this software and associated documentation files (the "Software"), to deal
+        # in the Software without restriction, including without limitation the rights
+        # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        # copies of the Software, and to permit persons to whom the Software is
+        # furnished to do so, subject to the following conditions:
+        #
+        # The above copyright notice and this permission notice shall be included in
+        # all copies or substantial portions of the Software.
+        #
+        # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        # SOFTWARE.
+        #endregion License ############################################################
+
+        #region DownloadLocationNotice #############################################
+        # The most up-to-date version of this script can be found on the author's
+        # GitHub repository at https://github.com/franklesniak/PowerShell_Resources
+        #endregion DownloadLocationNotice #############################################
+
+        if (Test-Path variable:\PSVersionTable) {
+            return ($PSVersionTable.PSVersion)
+        } else {
+            return ([version]('1.0'))
+        }
+    }
+
+    $strFolderPathContainingScript = ''
+    if (Test-Path variable:\PSScriptRoot) {
+        # $PSScriptRoot exists
+        if (-not [string]::IsNullOrEmpty($PSScriptRoot)) {
+            $strFolderPathContainingScript = $PSScriptRoot
+        }
+    }
+
+    if ([string]::IsNullOrEmpty($strFolderPathContainingScript)) {
+        # $PSScriptRoot does not exist or is empty
+        # Either PowerShell v1 or v2 is running, or there may not be a script running
+
+        $strScriptPath = ''
+        if (Test-Path variable:\HostInvocation) {
+            # Edge case for Sapien PrimalScript/PowerShell Studio
+            $strScriptPath = $HostInvocation.MyCommand.Path
+        } elseif (Test-Path variable:script:MyInvocation) {
+            $strScriptPath = (Get-Variable MyInvocation -Scope Script).Value.MyCommand.Definition
+        }
+
+        if (-not [string]::IsNullOrEmpty($strScriptPath)) {
+            # $strScriptPath would be the path to a script file, if we are, in fact,
+            # running inside a script.
+            # Otherwise, $strScriptPath would be the last command that was run, in
+            # which case Test-Path would fail.
+            if (Test-Path $strScriptPath) {
+                $strFolderPathContainingScript = (Split-Path $strScriptPath)
+            }
+        }
+
+        if ([string]::IsNullOrEmpty($strFolderPathContainingScript)) {
+            $strFolderPathContainingScript = (Get-Location).Path
+            if ($Host.Name -eq 'ConsoleHost' -or $Host.Name -eq 'ServerRemoteHost' -or $Host.Name -eq 'Windows PowerShell ISE Host' -or $Host.Name -eq 'Visual Studio Code Host') {
+                $versionPS = Get-PSVersion
+                $strMessage = 'Get-FolderPathContainingScript: There does not appear to be a script running; the current directory <' + $strFolderPathContainingScript + '> will be used.'
+                if ($versionPS.Major -ge 5) {
+                    Write-Information $strMessage
+                } else {
+                    Write-Host $strMessage
+                }
+            } else {
+                Write-Warning ('Get-FolderPathContainingScript: Powershell Host <' + $Host.Name + '> may not be compatible with this function, the current directory <' + $strFolderPathContainingScript + '> will be used.')
+            }
+        }
+    }
+
+    return $strFolderPathContainingScript
+}
+function Get-PSVersion {
+    # Returns the version of PowerShell that is running, including on the original
+    # release of Windows PowerShell (version 1.0)
+    #
+    # Example:
+    # Get-PSVersion
+    #
+    # This example returns the version of PowerShell that is running. On versions
+    # of PowerShell greater than or equal to version 2.0, this function returns the
+    # equivalent of $PSVersionTable.PSVersion
+    #
+    # The function outputs a [version] object representing the version of
+    # PowerShell that is running
+    #
+    # PowerShell 1.0 does not have a $PSVersionTable variable, so this function
+    # returns [version]('1.0') on PowerShell 1.0
+    #
+    # Version 1.0.20240917.0
+
+    #region License ############################################################
+    # Copyright (c) 2024 Frank Lesniak
+    #
+    # Permission is hereby granted, free of charge, to any person obtaining a copy
+    # of this software and associated documentation files (the "Software"), to deal
+    # in the Software without restriction, including without limitation the rights
+    # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    # copies of the Software, and to permit persons to whom the Software is
+    # furnished to do so, subject to the following conditions:
+    #
+    # The above copyright notice and this permission notice shall be included in
+    # all copies or substantial portions of the Software.
+    #
+    # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    # SOFTWARE.
+    #endregion License ############################################################
+
+    #region DownloadLocationNotice #############################################
+    # The most up-to-date version of this script can be found on the author's
+    # GitHub repository at https://github.com/franklesniak/PowerShell_Resources
+    #endregion DownloadLocationNotice #############################################
+
+    if (Test-Path variable:\PSVersionTable) {
+        return ($PSVersionTable.PSVersion)
+    } else {
+        return ([version]('1.0'))
+    }
+}
+#endregion Part 0: Load required functions ############################################
+
+#region Part 1: Copy scripts to the target system using remote PSSession ###########
+$strWin10IoTCoreDeviceIPOrHostname = "10.1.2.137"
+$strWin10IoTCoreUsername = "Administrator"
+$strCurrentFolder = Get-FolderPathContainingScript
+$strPathToInvokeX86ExeScript = Join-Path $strCurrentFolder 'Win10_IoT_Core'
+$strPathToInvokeX86ExeScript = Join-Path $strPathToInvokeX86ExeScript 'Invoke-x86Exe.ps1'
+$strPathToTestPowerShellModuleInstalledScript = Join-Path $strCurrentFolder 'Win10_IoT_Core'
+$strPathToTestPowerShellModuleInstalledScript = Join-Path $strPathToTestPowerShellModuleInstalledScript 'Test-PowerShellModuleInstalled.ps1'
+
+$versionPS = Get-PSVersion
+
+$credential = Get-Credential $strWin10IoTCoreUsername
+$PSSession = New-PSSession -ComputerName $strWin10IoTCoreDeviceIPOrHostname -Credential $credential
+
+$strWin10IoTCoreDownloadPathRoot = ("C:\Data\Users\" + $strWin10IoTCoreUsername + "\Downloads")
+
+if ($versionPS.Major -ge 5) {
+    Copy-Item $strPathToInvokeX86ExeScript -Destination $strWin10IoTCoreDownloadPathRoot -ToSession $PSSession -Force
+    Copy-Item $strPathToTestPowerShellModuleInstalledScript -Destination $strWin10IoTCoreDownloadPathRoot -ToSession $PSSession -Force
+} else {
+    Write-Error "Cannot copy a file to a PSSession on PowerShell v4 and previous; need to identify and write workaround!"
+}
+#endregion Part 1: Copy scripts to the target system using remote PSSession ###########
+
+#region Part 2: Connect to Windows 10 IoT Core and re-define functions #############
+Enter-PSSession -Session $PSSession
+
+function Get-DownloadFolder {
+    if (Test-Path variable:\HOME) {
+        $strBasePath = $HOME
+    } elseif (Test-Path env:\USERPROFILE) {
+        $strBasePath = $env:USERPROFILE
+    } else {
+        $strBasePath = $null
+    }
+
+    if ($null -ne $strBasePath) {
+        $strDownloads = Join-Path $strBasePath "Downloads"
+        if ((Test-Path $strDownloads) -eq $false) {
+            New-Item -Path $strDownloads -ItemType Directory | Out-Null
+        }
+        $strDownloads
+    } else {
+        Write-Warning "Get-DownloadFolder was unable to locate suitable folder path for downloads. Returning `$null"
+        $null
+    }
+}
+#endregion Part 2: Connect to Windows 10 IoT Core and re-define functions #############
+
+#region Part 3: Try some things on Windows 10 IoT Core #############################
+$strDownloadFolder = Get-DownloadFolder
+$strPathToInvokeX86ExeScript = Join-Path $strDownloadFolder 'Invoke-x86Exe.ps1'
+$strPathToTestPowerShellModuleInstalledScript = Join-Path $strDownloadFolder 'Test-PowerShellModuleInstalled.ps1'
+
+# This script downloads Streams for Sysinternals and attempts to run it:
+& $strPathToInvokeX86ExeScript
+# Did it work? Why or why not?
+
+# Hint:
+$env:PROCESSOR_ARCHITECTURE
+
+##########################################
+
+# This script checks if the PSCompression module is installed:
+& $strPathToTestPowerShellModuleInstalledScript
+# It's not! What happens if we try to install it (by following the suggested
+# instructions)?
+
+# Get more information about the installed version of PowerShell:
+$PSVersionTable
+# Note the PSVersion
+# ... and note the PSEdition!
+
+Exit-PSSession
+#endregion Part 3: Try some things on Windows 10 IoT Core #############################
+
+#region Part 4: Download PowerShell 7 for Windows 10 IoT Core and transfer it ######
+# ...back on the host system, we can download PowerShell 7 for Windows 10 IoT Core
+$strPathToInvokePowerShellDownloadScript = Join-Path $strCurrentFolder 'Invoke-PowerShellDownload.ps1'
+
+$results = & $strPathToInvokePowerShellDownloadScript -PreferZIP -Windows -ARM
+
+# Display the $results variable:
+$results
+
+# Transfer the downloaded file
+if ($versionPS.Major -ge 5) {
+    Copy-Item ($results.DownloadedFilePath) -Destination $strWin10IoTCoreDownloadPathRoot -ToSession $PSSession -Force
+} else {
+    Write-Error "Cannot copy a file to a PSSession on PowerShell v4 and previous; need to identify and write workaround!"
+}
+
+# The $results file has important information like the file name, etc., and this
+# variable is on the host, not the target system. How can we transfer it?
+# 🤔💭
+
+# Before we transfer it, let's update the paths to reflect the correct ones on the
+# remote host
+$strHostFolderPath = $results.FolderPathContainingDownload
+$results.FolderPathContainingDownload = $strWin10IoTCoreDownloadPathRoot
+$results.DownloadedFilePath = Join-Path $strWin10IoTCoreDownloadPathRoot ($results.DownloadedFileName)
+
+# Convert it to JSON:
+$strPathToDownloadResultsJSONFile = Join-Path $strCurrentFolder 'PowerShellDownloadResults.json'
+$results | ConvertTo-Json | Out-File -FilePath $strPathToDownloadResultsJSONFile -Force
+
+# Transfer it
+if ($versionPS.Major -ge 5) {
+    Copy-Item $strPathToDownloadResultsJSONFile -Destination $strWin10IoTCoreDownloadPathRoot -ToSession $PSSession -Force
+} else {
+    Write-Error "Cannot copy a file to a PSSession on PowerShell v4 and previous; need to identify and write workaround!"
+}
+
+# Put the downloaded file info back on the host just in case
+$results.FolderPathContainingDownload = $strHostFolderPath
+$results.DownloadedFilePath = Join-Path $strHostFolderPath ($results.DownloadedFileName)
+#endregion Part 4: Download PowerShell 7 for Windows 10 IoT Core and transfer it ######
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #region DownloadPowerShell7ForWin10IoTCore
 ##############################################################################################
 # Part 1 - Download PowerShell 7 for Windows 10 IoT Core
